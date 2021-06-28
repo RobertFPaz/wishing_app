f78613b62c31e128a812ac5bf323b1f827e71bcf
Robert Paz <rtfp0102@gmail.com>
2021-06-27 23:06:28 -0700

    removed file

--- pyBeltProj/settings.py
+++ pyBeltProj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from . import keys
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = keys.SECRET_KEY
+SECRET_KEY = '9*g7gi0hxpt))+s^m_r(yg_fme8@26wlrtj^1penb2b^bd(^@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
